74f663bfaa3b80916a4057ea02e57f1e9f0924c2
jackhkmatthews <jackhkmatthews@gmail.com>
2016-10-12 17:55:37 +0100

    seperated out controllers and placed buttons and css

---
+++ public/scripts/controllers/CollapseDemoCtrl.js
@@ -0,0 +1,6 @@
+angular.module('tubeVisualiserApp').controller('CollapseDemoCtrl', function ($scope) {
+  $scope.isNavCollapsed = true;
+  $scope.isCollapsed = true;
+  $scope.isCollapsed2 = true;
+  $scope.isCollapsedHorizontal = false;
+});
