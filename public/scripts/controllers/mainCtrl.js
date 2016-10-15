6c960042cf90a0100e910d03b76ee5a945991201
jackhkmatthews <jackhkmatthews@gmail.com>
2016-10-15 21:19:30 +0100

    succesfully created styles object

--- public/scripts/controllers/mainCtrl.js
+++ public/scripts/controllers/mainCtrl.js
@@ -7,33 +7,30 @@ function getRgbColor () {
   return rgbColor;
 };
 
-function getRbgLinearGradient () {
+function getGradientStylesObject () {
   rgbColor1 = getRgbColor();
   rgbColor2 = getRgbColor();
-  rbgLinearGradient = 'linear-gradient(' + rgbColor1 + ',' + rgbColor2 + ')';
-  return rbgLinearGradient
+  linearGradient = 'linear-gradient(' + rgbColor1 + ',' + rgbColor2 + ')';
+  gradientStylesObject = {
+    "rgbColor1" : rgbColor1,
+    "rgbColor2" : rgbColor2,
+    "linearGradient" : linearGradient,
+    "ngStyleSyntax" : {
+        background: linearGradient,
+      }
+    };
+  return gradientStylesObject;
 };
 
-function getGradientStylesObject () {
-  GradientStylesObject = {
-    "rgbColor1" : getRgbColor(),
-    "rgbColor2" : getRgbColor(),
-    "linear-gradient" : 'linear-gradient(' + this.rgbColor1 + ',' + this.rgbColor2 + ')'
-  }
-  return GradientStylesObject;
-};
+angular.module('tubeVisualiserApp')
+.controller('mainCtrl', function($scope) {
+  $scope.gradientStylesObject = getGradientStylesObject();
+  $scope.getGradientStylesObject = getGradientStylesObject;
+});
 
 // angular.module('tubeVisualiserApp')
 // .controller('mainCtrl', function($scope) {
-//   $scope.gradientContainerStyleInfo
 //   $scope.gradientContainerStyles = {"background":getRbgLinearGradient()};
 //   $scope.getRgbColor = getRgbColor;
 //   $scope.getRgbLinearGradient = getRbgLinearGradient;
 // });
-
-angular.module('tubeVisualiserApp')
-.controller('mainCtrl', function($scope) {
-  $scope.gradientContainerStyles = {"background":getRbgLinearGradient()};
-  $scope.getRgbColor = getRgbColor;
-  $scope.getRgbLinearGradient = getRbgLinearGradient;
-});
